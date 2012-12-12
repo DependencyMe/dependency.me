8117dd56337d97f43adb08b9c2762251dc43649d
Halleck45 <jeanfrancois@lepine.pro>
2012-12-12 16:09:43 +0100

    Structure of controllers in the web app

--- src/Hal/ReleaseWebBundle/Controller/OwnerController.php
+++ src/Hal/ReleaseWebBundle/Controller/OwnerController.php
@@ -3,13 +3,20 @@
 namespace Hal\ReleaseWebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
+/**
+ * @Route("/owner")
+ */
 class OwnerController extends Controller
 {
-
+    /**
+     * @Route("/register", name="owner.register")
+     */
     public function registerAction()
     {
-
 
     }
 
--- src/Hal/ReleaseWebBundle/Controller/PackageController.php
+++ src/Hal/ReleaseWebBundle/Controller/PackageController.php
@@ -3,13 +3,51 @@
 namespace Hal\ReleaseWebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use JMS\SecurityExtraBundle\Annotation\SecureParam;
 
+/**
+ * @Route("/package")
+ */
 class PackageController extends Controller
 {
     /**
+     * @Secure(roles="ROLE_OWNER")
+     * @Template
+     * @Route("/add", name="package.add")
+     */
+    public function addPackageAction()
+    {
+
+    }
+
+    /**
+     * @Secure(roles="ROLE_OWNER")
+     * @SecureParam(name="package", permissions="OWNER")
+     * @Template
+     * @Route("/remove/{id}", name="package.remove")
+     */
+    public function removePackage(\Hal\ReleaseBundle\Entity\Package $package)
+    {
+
+    }
+
+    /**
      * @Template(vars={$package})
+     * @Route("/display/{id}", name="package.display")
      */
     public function displayPackageAction(\Hal\ReleaseBundle\Entity\Package $package)
     {
+    }
+
+    /**
+     * @Template
+     * @Route("/image/{id}", name="package.image")
+     */
+    public function displayImageStateAction(\Hal\ReleaseBundle\Entity\Package $package)
+    {
+
     }
 }
