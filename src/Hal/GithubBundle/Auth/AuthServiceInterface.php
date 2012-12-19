92503353cdf74b686486bd160483f21a04a8ae19
Halleck45 <jeanfrancois@lepine.pro>
2012-12-19 17:32:45 +0100

    fixes bugs in github authentification workflow

--- src/Hal/GithubBundle/Auth/AuthServiceInterface.php
+++ src/Hal/GithubBundle/Auth/AuthServiceInterface.php
@@ -1,67 +1,8 @@
 <?php
-
-use Symfony\Component\HttpFoundation\Request;
+namespace Hal\GithubBundle\Auth;
 use Hal\GithubBundle\Entity\AuthentifiableInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class AuthService
+interface AuthServiceInterface
 {
-    private $clientId;
-    private $request;
-    private $redirectUri;
-    private $clientSecret;
-
-    function __construct(Request $request, $appClientId, $appClientSecret, $redirectUri)
-    {
-        $this->clientId = $appClientId;
-        $this->clientSecret = $appClientSecret;
-        $this->request = $request;
-        $this->redirectUri = $redirectUri;
-    }
-
-
-    public function authentificate(AuthentifiableInterface $user)
-    {
-        // Pattern Template
-
-        // 1. ask for temporary code
-        if (null === $user->getTemporaryCode()) {
-            return $this->getTemporaryAccess($user);
-        }
-
-        // 2. ask for permanent access (with token)
-        if (null === $user->getPermanentAccessToken()) {
-            return $this->getPermanentAccess($user);
-        }
-
-        return;
-    }
-
-    private function getTemporaryAccess(AuthentifiableInterface $user)
-    {
-        if ($this->request->isMethod('POST')) {
-            $code = $this->request->get('code');
-            if (null !== $code) {
-                $user->setTemporaryCode($code);
-            }
-        }
-
-        return new RedirectResponse(sprintf('https://github.com/login/oauth/authorize?client_id=%1$s&redirect_uri=%2$s'
-            , $this->clientId, $this->redirectUri
-        ));
-    }
-
-    private function getPermanentAccess(AuthentifiableInterface $user)
-    {
-        if ($this->request->isMethod('POST')) {
-            $token = $this->request->get('access_token');
-            if (null !== $token) {
-                $user->setPermanentAccessToken($token);
-            }
-        }
-
-        return new RedirectResponse(sprintf('https://github.com/login/oauth/access_token?client_id=%1$s&redirect_uri=%2$s&client_secret=%3$s&code=%4$s'
-            , $this->clientId, $this->redirectUri, $this->clientSecret, $user->getTemporaryCode()
-        ));
-    }
+    public function authentificate(AuthentifiableInterface $user);
 }
--- src/Hal/GithubBundle/Controller/AuthController.php
+++ src/Hal/GithubBundle/Controller/AuthController.php
@@ -1,66 +1,32 @@
 <?php
 
-namespace Hal\ReleaseWebBundle\Controller;
+namespace Hal\GithubBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use JMS\SecurityExtraBundle\Annotation\SecureParam;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Hal\GithubBundle\Auth\AuthServiceInterface;
 
 /**
- * @Route("/package")
+ * @Route("/github")
  */
 class PackageController extends Controller
 {
     /**
-     * @Secure(roles="ROLE_OWNER")
-     * @Template
-     * @Route("/add", name="package.add")
+     * @Route("/auth", name="github.auth")
      */
-    public function addPackageAction()
+    public function authentificateAction()
     {
-
+        $service = $this->get('hal.github.auth.service');
+        $user = $this->get('session')->get('github.auth.user');
+        if (!$user) {
+            $user = new \Hal\GithubBundle\Entity\Authentifiable();
+        }
+        $service->authentificate($user);
+        return new RedirectResponse('home');
     }
 
-    /**
-     * @Route("/search/{searchedTerm}", name="package.search")
-     * @Template
-     */
-    public function searchAction($searchedTerm) {
-        $service = $this->get('hr.package.service');
-        return array(
-            'packages' => $service->getMatchingPackages($searchedTerm)
-            ,'searchedTerm' => $searchedTerm
-        );
-    }
-
-    /**
-     * @Secure(roles="ROLE_OWNER")
-     * @SecureParam(name="package", permissions="OWNER")
-     * @Template
-     * @Route("/remove/{id}", name="package.remove")
-     */
-    public function removePackage(\Hal\ReleaseBundle\Entity\Package $package)
-    {
-
-    }
-
-    /**
-     * @Template(vars={"package"})
-     * @Route("/display/{id}", name="package.display")
-     */
-    public function displayPackageAction(\Hal\ReleaseBundle\Entity\Package $package)
-    {
-
-    }
-
-    /**
-     * @Template
-     * @Route("/image/{id}", name="package.image")
-     */
-    public function displayImageStateAction(\Hal\ReleaseBundle\Entity\Package $package)
-    {
-
-    }
 }
